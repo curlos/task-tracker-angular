24aa34e739d9f61fc01c0768321b29e399749ce0
Carlos Martinez <curlosmart@gmail.com>
2022-03-09 15:08:01 -0500

    Add default data and base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
