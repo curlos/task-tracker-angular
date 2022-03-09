331d880e38ba08e1886a8c93d0fd63bd4f0c9e4f
Carlos Martinez <curlosmart@gmail.com>
2022-03-09 16:49:34 -0500

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
